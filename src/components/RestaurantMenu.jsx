598661835f35b34dbafad7e263504c89b722d0b5
mohdathar01 <atharchaudhary2000@gmail.com>
2024-09-05 16:58:50 +0530

    every component data

--- src/components/RestaurantMenu.jsx
+++ src/components/RestaurantMenu.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
+import { CHILD_OF_CARD_CID_RESMENU_COMP } from "../utils/constant";
 
 const RestaurantMenu = () => {
-  const [resInfo, setResInfo] = useState(null);
+  const [resInfo, setResInfo] = useState();
 
   useEffect(() => {
     fetchMenu();
@@ -11,31 +12,44 @@ const RestaurantMenu = () => {
 
   const fetchMenu = async () => {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.65200&lng=77.16630&restaurantId=655882&catalog_qa=undefined&submitAction=ENTER"
+      "https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.65200&lng=77.16630&restaurantId=253714&catalog_qa=undefined&submitAction=ENTER"
     );
     const json = await data.json();
-    console.log(json);
-    console.log(json?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card?.categories[0]?.itemCards[0]?.card?.info?.name)
+    console.log(json.data);
+
+    console.log(
+      resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card
+        ?.card?.itemCards[0]?.card?.info || {}
+    );
     setResInfo(json.data);
   };
-  // const {name}=(resInfo?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card?.categories[0]?.itemCards[0]?.card?.info?.name); 
+  const { name, price, imageId } =
+    resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card
+      ?.itemCards[0]?.card?.info || {};
 
   return resInfo === null ? (
     <Shimmer />
   ) : (
-    <div>
-      <h2>
-         
-        {name}
-
-        
-      </h2>
-      <h3>Menu</h3>
-      <ul>
-        <li>Biryani</li>
-        <li>Burgers</li>
-        <li>Diet Coke</li>
-      </ul>
+    <div className="resmenu">
+      <div>
+        <img
+          className="imgofresmenu"
+          src={CHILD_OF_CARD_CID_RESMENU_COMP + imageId}
+          alt=""
+        />
+      </div>
+      <div className="nameofresmenu">
+        <h2 className="namee">{name}</h2>
+      </div>
+      <div className="sweets">
+        <h3>Laddu</h3>
+        <h3>Gulab Jamun</h3>
+        <h3>Samosa</h3>
+        <h3>Kuch nhi</h3>
+      </div>
+      <div>
+        <p>{price / 100}.00 for two persons</p>
+      </div>
     </div>
   );
 };
--- src/utils/constant.jsx
+++ src/utils/constant.jsx
@@ -1,2 +1,4 @@
 export const CLOUDNARY_IMAGE_ID =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+export const CHILD_OF_CARD_CID_RESMENU_COMP =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/";
