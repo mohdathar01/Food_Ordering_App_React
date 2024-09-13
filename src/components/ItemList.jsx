00eca627d20d4a695a9a4d0b6c84c76e1d329af7
unknown <atharchaudhary2000@gmail.com>
2024-09-13 09:51:21 +0530

    Resolved Api Issue

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,19 +1,25 @@
 import React from 'react'
+import {CLOUDNARY_IMAGE_ID} from "../utils/constant";
 
 const ItemList = ({items}) => {
-    // console.log(items?.card?.info);
+    console.log("itemcard wala console",items);
+    
   return (
-    <div> 
+    <div className='maindivofitem'> 
         {items.map((item)=>(
             <div key={item.card.info.id} className='itemsinsideone'> 
             <div className='divofnameprice'>
                 <span className='itemcardname'>{item.card.info.name}</span> 
-                <span className='itemcardprice'>{"-₹"}{item.card.info.price?item.card.info.price/100 : item.card.info.defaultPrice/100}</span>
+                <span className='itemcardprice'>{"-₹"}{item.card.info.price ? item.card.info.price/100 : item.card.info.defaultPrice/100}</span>
             </div>
+            <div className='divofpimage'>
             <p className='itemdescription'>
             {item.card.info.description}
             </p>
-            
+            <div className='divisimgbtn'>
+                <img className='itemimage' src={CLOUDNARY_IMAGE_ID + item.card.info.imageId} alt="" />
+                <button className='btnofitem'>Add +</button></div>
+            </div>
             </div>
         ))}
     </div>
