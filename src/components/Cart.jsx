a5c7c1f0bcd2065aebb7b8579ae1596622b0d8b2
unknown <atharchaudhary2000@gmail.com>
2024-09-25 09:52:16 +0530

    Redux

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,11 +1,34 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import ItemList from "./ItemList";
+import { clearCart } from "../utils/cartSlice";
 
 const Cart = () => {
-  const cartItems = useSelector((store) => store.cart.items);
+
+ const cartItems = useSelector((store) => store.cart.items);
+
+  // both below are do same thing but the above is targating a small portion of the store so always go with above when using redux
+  // const store = useSelector((store) => store.cart.items);
+  // const cartItems=store.cart.items;
+
+  const dispatch=useDispatch();
+
+const handleClearCart=()=>{
+  dispatch(clearCart());
+}
+
+
   return (
     <div className="cartmaindiv">
-      <div className="mcontainercart"> Your Cart</div>
+      <div className="mcontainercart">
+         <h3>Your Cart
+          </h3>
+          <button className="clrcaartbtn" onClick={handleClearCart}>
+       Clear Cart
+        </button>
+          </div>
+          {cartItems.length===0 &&(<h1>Cart is empty.please Add Item into the Cart🍔🍕🍟</h1>)}
+
+      
       <div className="cardaddeditems">
         <ItemList items={cartItems} />
       </div>
