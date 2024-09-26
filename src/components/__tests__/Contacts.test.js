7ea9855cc694bafe48d83816fac11becf2a782e8
mohdathar01 <atharchaudhary2000@gmail.com>
2024-09-26 17:13:51 +0530

    Testing of app

--- src/components/__tests__/Contacts.test.js
+++ src/components/__tests__/Contacts.test.js
@@ -1,10 +1,30 @@
 import Contacts from "../Contacts";
-import { render,screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
-test("Should load contact us component",()=>{
+test("Should load heading inside contact us component", () => {
+  render(<Contacts />);
+  const heading = screen.getByRole("heading");
+  expect(heading).toBeInTheDocument();
+});
 
-    render(<Contacts/>);
-    const heading=screen.getByRole("heading");
-    expect(heading).toBeInTheDocument();
-})
+test("Should load button inside contact us component", () => {
+  render(<Contacts />);
+  const button = screen.getByRole("button");
+  expect(button).toBeInTheDocument();
+});
+
+//for single input box
+test("Should load button inside contact us component", () => {
+  render(<Contacts />);
+  const input = screen.getByPlaceholderText("Name");
+  expect(input).toBeInTheDocument();
+});
+
+//for multiple input or tag use below
+test("Should load button inside contact us component", () => {
+  render(<Contacts />);
+  const inputBoxes = screen.getAllByRole("textbox");
+  //   expect(inputBoxes).toBeInTheDocument();
+  console.log(inputBoxes);
+});
